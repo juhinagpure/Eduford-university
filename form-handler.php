880428cdbed56ce5a9e6d19b53d5e83373825653
JUHI NAGPURE <juhinagpure143@gmail.com>
2023-01-29 20:03:45 +0530

    added new file form-handler.php

---
+++ form-handler.php
@@ -0,0 +1,25 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email';];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'juhinagpure143@gmail.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name : $name.\n".
+              "User Email: $visitor_email.\n".
+              "Subject:$subject.\n".
+              "User Message:$message.\n";
+$to = 'juhinagpure143@gmail.com';
+
+$headers = "form: $email_from \r\n";
+
+$headers . ="Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location:contact.html");
+
+?>
